a26f90446efb224141c1db8e1ac2742ea17805f4
Leo Kewitz <leo@opencollective.com>
2022-01-04 16:43:44 -0300

    fix: reset Captcha if createOrder request fails

--- components/contribution-flow/index.js
+++ components/contribution-flow/index.js
@@ -139,6 +139,7 @@ class ContributionFlow extends React.Component {
     super(props);
     this.mainContainerRef = React.createRef();
     this.formRef = React.createRef();
+    this.captchaRef = React.createRef();
     this.state = {
       error: null,
       stripe: null,
@@ -220,7 +221,7 @@ class ContributionFlow extends React.Component {
 
       return this.handleOrderResponse(response.data.createOrder, stepProfile.email);
     } catch (e) {
-      this.setState({ isSubmitting: false });
+      this.handleError();
       this.showError(getErrorFromGraphqlException(e));
     }
   };
@@ -239,10 +240,18 @@ class ContributionFlow extends React.Component {
     }
   };
 
+  handleError = message => {
+    this.setState({ isSubmitting: false, error: message });
+    if (isCaptchaEnabled() && !this.props.LoggedInUser) {
+      this.setState({ stepProfile: set(this.state.stepProfile, 'captcha', null) });
+      this.captchaRef?.current?.resetCaptcha();
+    }
+  };
+
   handleStripeError = async (order, stripeError, email, guestToken) => {
     const { message, account, response } = stripeError;
     if (!response) {
-      this.setState({ isSubmitting: false, error: message });
+      this.handleError(message);
     } else if (response.paymentIntent) {
       const isAlipay = response.paymentIntent.allowed_source_types[0] === 'alipay';
       const stripe = await getStripe(null, account);
@@ -252,14 +261,14 @@ class ContributionFlow extends React.Component {
           })
         : await stripe.handleCardAction(response.paymentIntent.client_secret);
       if (result.error) {
-        this.setState({ isSubmitting: false, error: result.error.message });
+        this.handleError(result.error.message);
       } else if (result.paymentIntent && result.paymentIntent.status === 'requires_confirmation') {
         this.setState({ isSubmitting: true, error: null });
         try {
           const response = await this.props.confirmOrder({ variables: { order: { id: order.id }, guestToken } });
           return this.handleOrderResponse(response.data.confirmOrder, email);
         } catch (e) {
-          this.setState({ isSubmitting: false, error: e.message });
+          this.handleError(e.message);
         }
       }
     }
@@ -625,7 +634,7 @@ class ContributionFlow extends React.Component {
             return true;
           } else {
             const isCompleted = Boolean(noPaymentRequired || stepPayment);
-            if (isCaptchaEnabled() && !stepProfile.captcha) {
+            if (isCaptchaEnabled() && !stepProfile.captcha && !LoggedInUser) {
               this.showError('Captcha is required.');
               return false;
             } else if (isCompleted && stepPayment?.key === NEW_CREDIT_CARD_KEY) {
@@ -835,7 +844,7 @@ class ContributionFlow extends React.Component {
                     disabledPaymentMethodTypes={this.props.disabledPaymentMethodTypes}
                     hideCreditCardPostalCode={this.props.hideCreditCardPostalCode}
                   />
-                  {currentStep.name === STEPS.PAYMENT && isCaptchaEnabled() && (
+                  {isCaptchaEnabled() && currentStep.name === STEPS.PAYMENT && !LoggedInUser && (
                     <Flex mt={40} justifyContent="center">
                       <Captcha
                         ref={this.captchaRef}
